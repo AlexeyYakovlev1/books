f80358cf8f2552fba00ff93e1ac76da5a75cda2a
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2023-02-25 21:19:58 +0300

    call to api for book page

--- scripts/logics/Books.js
+++ scripts/logics/Books.js
@@ -1,14 +1,15 @@
 class Books {
 	constructor() {
 		this.LIMIT = 50;
-		this.API_URL = `https://example-data.draftbit.com/books?_limit=${this.LIMIT}`;
+		this.API_URL = `https://example-data.draftbit.com/books`;
 		this.loader = false;
 	}
 
+	// get books (limit)
 	async getBooks() {
 		this.loader = true;
 
-		const response = await fetch(this.API_URL, { method: "GET" });
+		const response = await fetch(`${this.API_URL}?_limit=${this.LIMIT}`, { method: "GET" });
 		const data = await response.json();
 
 		this.loader = false;
@@ -16,10 +17,24 @@ class Books {
 		return data;
 	}
 
+	// get book by id
+	async getOneBook(id) {
+		this.loader = true;
+
+		const response = await fetch(`${this.API_URL}?id=${id}`, { method: "GET" });
+		const data = await response.json();
+
+		this.loader = false;
+
+		return data[0];
+	}
+
+	// return limit
 	get getLimit() {
 		return this.LIMIT;
 	}
 
+	// return loader
 	get getLoader() {
 		return this.loader;
 	}
