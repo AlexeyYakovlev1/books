436a6b23f81aaef74f1b4c002e419ff00cc652f8
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2023-03-12 10:10:25 +0300

    update: mobile menu, height cover in book page, position cross in fovourite page

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,15 @@
 const $mobileMenuIcon = document.querySelector(".mobile__menu--icon");
 const $mobileMenuNav = document.querySelector(".mobile__menu--nav");
+const $mobileMenuClose = document.querySelector(".mobile__menu--close");
 
 $mobileMenuIcon.addEventListener("click", () => {
-	$mobileMenuNav.classList.toggle("hidden");
+	$mobileMenuNav.classList.remove("hidden");
+	$mobileMenuClose.classList.remove("hidden");
+	$mobileMenuIcon.classList.add("hidden");
+});
+
+$mobileMenuClose.addEventListener("click", () => {
+	$mobileMenuNav.classList.add("hidden");
+	$mobileMenuClose.classList.add("hidden");
+	$mobileMenuIcon.classList.remove("hidden");
 });
